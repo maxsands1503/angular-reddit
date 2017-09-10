8347c0a10ee60df621095d42bbccd4d13bad7dea
Maxwell Sands <maxsands1503@icloud.com>
2017-09-10 14:27:00 -0600

    added new component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  public addArticle(title: HTMLInputElement, link: HTMLInputElement){
-    
+  public addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(`Adding article ${title.value} with the following link ${link.value}`);
+    return false;
   }
 }
